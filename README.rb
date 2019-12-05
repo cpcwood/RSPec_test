bd8cb4c53e7acdf1c2c42261c0fa0e5bef851097
cpcwood <cpcwood@outlook.com>
2019-12-05 16:04:07 +0000

    Readme and test files added

---
+++ README.rb
@@ -0,0 +1,10 @@
+# RSpec Test Files #
+
+## Overview ##
+
+Repository for learning how rspec system works
+
+
+## Includes ##
+
+Basic RSpec test script for name greeting
---
+++ lib/greeter.rb
@@ -0,0 +1,3 @@
+def greet(name)
+  return "Hello, #{name}, how are you today?"
+end
